83cfe88a9907abc330edb74f6f12c2f7deaa246d
Panagiotis Moliatsidis <moliaspanos@gmail.com>
2024-10-24 23:27:49 +0300

    Creation of property owner model

---
+++ Technico/Models/PropertyOwner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Technico.Enums;
+
+namespace Technico.Models;
+
+public class PropertyOwner
+{
+    public int VATNumber { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public int TelephoneNumber { get; set; }
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+
+    public UserType Role { get; set; }
+
+    public List<PropertyItem>? PropertyItems { get; set; }
+    public List<PropertyOwner>? RepairDone { get; set; }
+    //LIST OF REPAIRS
+}
