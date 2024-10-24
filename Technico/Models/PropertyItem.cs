7ec4ce13fb86ee3614a155b1875f7c8d0ae7941c
Panagiotis Moliatsidis <moliaspanos@gmail.com>
2024-10-24 23:39:38 +0300

    Creation of Property Item and Repair Models.

---
+++ Technico/Models/PropertyItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technico.Models;
+
+public class PropertyItem
+{
+    public int PropertIdNumber { get; set; }
+    public string PropertyAddress { get; set; } = null! ;
+    public int YearOfConstruction { get; set; }
+    public enum TypeOfProperty { DetachedHouse, Maisonet, ApartmentBuilding }
+    public int VATNumber { get; set; }
+                                                        
+
+}
+
---
+++ Technico/Models/Repair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technico.Models;
+
+public class Repair
+{
+    public DateTime StartDate { get; set; }
+    public enum TypeOfRepair { Painting, Insulation, Frames, plumbing, electricalWork }
+
+    public string RepairDescription { get; set; } = null!;
+    public string RepairAddress { get; set; } = null!;
+
+    public enum StatusOfTheRepair { Pending, Inprogress, Complete , Default }
+    public int CostOfRepair { get; set; }
+    public int OwnerVAT {  get; set; }
+    
+
+    
+}
